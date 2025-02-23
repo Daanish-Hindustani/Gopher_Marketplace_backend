42ebf2c729129b9dcdd915ba60508b3002edf5d4
Daanish Hindustani <daanishhindustani@yahoo.com>
2025-02-22 20:49:17 -0600

    updated docker compose

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
-
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
+        host="0.0.0.0",  # Bind to all available network interfaces
+        port=8000,        # Ensure this matches the exposed port in Docker
         reload=True if config.ENVIRONMENT != "production" else False,
         workers=1,
     )
